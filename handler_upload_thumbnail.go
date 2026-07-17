45cd63dc9c326cc1d8f54f85082b3a93b893e4bc
EdwardVNguyen <edwardnguyen568@gmail.com>
2026-07-17 17:42:32 -0500

    thumbnail multipart upload feature

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +29,46 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20 // 10 mb
+	if err := r.ParseMultipartForm(maxMemory); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse multipart form", err)
+		return
+	}
+	
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read file data", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized to get video as current user is not owner", err)
+		return
+	}
+
+	thumbnail := thumbnail{
+		data: imageData,
+		mediaType: header.Header.Get("Content-Type"),
+	}
+	videoThumbnails[video.ID] = thumbnail
+
+	endpoint := fmt.Sprintf(`http://localhost:%s/api/thumbnails/%s`, cfg.port, videoIDString)
+	video.ThumbnailURL = &endpoint
+
+	if err := cfg.db.UpdateVideo(video); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
