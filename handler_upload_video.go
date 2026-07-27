562abd038bb54c2b90128d04d56189bc428b9bdc
EdwardVNguyen <edwardnguyen568@gmail.com>
2026-07-27 13:56:56 -0500

    I was in fact wrong, I did not get video upload first try. Should work now :)

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -17,9 +17,11 @@ import (
 )
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
-	const uploadLimit = 1 << 30 // 1 GB
+	// set request upload limit to 1 GB
+	const uploadLimit = 1 << 30 
 	r.Body = http.MaxBytesReader(w, r.Body, uploadLimit)
 
+	// in order to update video's metadata later on
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
@@ -27,6 +29,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// authenticate user
 	token, err := auth.GetBearerToken(r.Header)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
@@ -38,6 +41,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// ok, we get the video metadata from the database (we also validate if the video is from the user)
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Could not get video", err)
@@ -48,20 +52,28 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 		
-	videoFile, _, err := r.FormFile("video")
+	// formfile gets the first file that has the form key "video"
+	videoFile, videoFileHeader, err := r.FormFile("video")
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Could not get form file from request", err)
 		return
 	}
 	defer videoFile.Close()
 
-	const contentType = "video/mp4"
+	// validate that the file is indeed of mediatype video/mp4
+	contentType := videoFileHeader.Header.Get("Content-Type")
 	mediaType, _, err := mime.ParseMediaType(contentType); 
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Could not parse mediatype", err)
 		return
 	}
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "media type is not video/mp4 so invalid", err)
+		return
+	}
 
+	// We make a temporary file since formfile does not give us the operations needed so that we can send the video file to aws
+	// so we have to copy the video file in a temporary file which does give us the operations and info needed
 	const tempFileName = "tubely-upload.mp4"
 	tempFile, err := os.CreateTemp("", tempFileName)
 	if err != nil {
@@ -71,16 +83,18 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	defer os.Remove(tempFileName)
 	defer tempFile.Close()
 
-if _, err := io.Copy(tempFile, videoFile); err != nil {
+	if _, err := io.Copy(tempFile, videoFile); err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Could not copy video file to temporary file", err)
 		return
 	}
 
+	// reset temp file pointer since its EOF, we want to point it back to the start of the file so that our we can send the file onto the bucket
 	if _, err := tempFile.Seek(0,io.SeekStart); err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Could not reset tempFile starting pointer", err)
 		return
 	}
 
+	// make a unique arbitrary name for the user's video
 	key := make([]byte, 32)
 	rand.Read(key)
 	encodedStr := base64.RawURLEncoding.EncodeToString(key)
@@ -88,14 +102,19 @@ if _, err := io.Copy(tempFile, videoFile); err != nil {
 	fileExtension := strings.Split(contentType, "/")[1]
 	s3Key := fmt.Sprintf(`%s.%s`, encodedStr, fileExtension)
 	
+	// put the file onto aws s3 bucket
 	// https://docs.aws.amazon.com/code-library/latest/ug/go_2_s3_code_examples.html
-	cfg.s3Client.PutObject( context.TODO(), &s3.PutObjectInput {
-		Bucket: &cfg.s3Bucket,
-		Key: &s3Key,
-		Body: videoFile,
-		ContentType: &mediaType,
-	})
+	if _, err := cfg.s3Client.PutObject( context.TODO(), &s3.PutObjectInput {
+			Bucket: &cfg.s3Bucket,
+			Key: &s3Key,
+			Body: tempFile,
+			ContentType: &mediaType,
+		}); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error uploading file to bucket", err)		
+		return
+	}
 
+	// update the video's metadata
 	updatedVideoURL := fmt.Sprintf(`https://%s.s3.%s.amazonaws.com/%s`, cfg.s3Bucket, cfg.s3Region, s3Key)
 	video.VideoURL = &updatedVideoURL
 	if err := cfg.db.UpdateVideo(video); err != nil {
